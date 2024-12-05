8080e83455d4c03d1432af48f357557f568ec753
yanis.hlali <yanis.hlali@gmail.com>
2024-12-05 15:07:28 +0100

    Add ESLint configuration and lint script to package.json

---
+++ eslint.config.js
@@ -0,0 +1,18 @@
+/** @type {import('eslint').Linter.Config} */
+module.exports = {
+    languageOptions: {
+      parserOptions: {
+        ecmaVersion: 2020,
+        sourceType: 'module',
+      },
+    },
+    plugins: {
+      vue: require('eslint-plugin-vue'),
+    },
+    rules: {
+      'no-console': 'warn',
+      'no-debugger': 'warn',
+      'vue/no-unused-vars': 'error',
+    },
+  };
+  
